4a0c3c22293a909db15c4d4a1ef5277f174ecd0c
Sam Johnson <sam@durosoft.com>
2018-05-28 08:13:31 -0400

    add specs for TempDir.create

--- spec/tempdir_spec.cr
+++ spec/tempdir_spec.cr
@@ -15,6 +15,16 @@ describe TempDir do
 
   it "creates directory prefixes correctly" do
     dir = TempDir.new "great-prefix"
+    File.exists?(dir.path).should eq true
     File.basename(dir.path).starts_with?("great-prefix").should eq true
+  end
+
+  it "works correctly with the block-style create option" do
+    path_unscoped = ""
+    TempDir.create do |path|
+      File.exists?(path).should eq true
+      path_unscoped = path
+    end
+    File.exists?(path_unscoped).should eq false
   end
 end
