0aa70dd89dcf547522442fa7f948c60771042b86
Sam Johnson <sam@durosoft.com>
2018-05-27 22:23:02 -0400

    add basic functionality

--- src/tempdir.cr
+++ src/tempdir.cr
@@ -1,6 +1,30 @@
-require "./tempdir/*"
+require "file_utils"
 
-# TODO: Write documentation for `Tempdir`
-module Tempdir
-  # TODO: Put your code here
+class TempDir
+  getter path : String
+
+  def initialize
+    @path = tempdir
+  end
+
+  def initialize(prefix)
+    @path = tempdir prefix
+  end
+
+  def delete
+    FIleUtils.rm_rf @path
+  end
+
+  def to_s
+    @path
+  end
+
+  private def tempdir(prefix="")
+    path = "/tmp/#{prefix ? prefix + "-" : ""}#{Random.rand(1000000000000000..9999999999999999)}"
+    FileUtils.mkdir_p path
+    at_exit do
+      FileUtils.rm_rf path
+    end
+    path
+  end
 end
--- src/tempdir/version.cr
+++
@@ -1,3 +0,0 @@
-module Tempdir
-  VERSION = "0.1.0"
-end
